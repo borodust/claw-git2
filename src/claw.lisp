6e14d99fda8165591a51d258bbae7b457ca18253
Pavel Korolev <dev@borodust.org>
2021-08-29 16:14:02 +0300

    Switch to MSVC target for Windows bindings

--- src/claw.lisp
+++ src/claw.lisp
@@ -4,7 +4,7 @@
                   (:includes :git2-includes)
                   (:targets ((:and :x86-64 :linux) "x86_64-pc-linux-gnu")
                             ((:and :x86-64 :darwin) "x86_64-apple-darwin-gnu")
-                            ((:and :x86-64 :windows) "x86_64-w64-mingw32"))
+                            ((:and :x86-64 :windows) "x86_64-pc-windows-msvc"))
                   (:persistent t)
                   (:include-definitions "^(git|GIT)_\\w+"))
   :in-package :%git
